7bb9e2613ca0f103789e09a971fea7618fa10a53
Serger Rutrkovsky <dr.bibelman@gmai.com>
2023-09-18 00:42:32 +0300

    first commit

---
+++ roud.js
@@ -0,0 +1,174 @@
+// @ts-check
+const mainTasks = [
+  {
+    id: 1,
+    task: "Html",
+  },
+  {
+    id: 2,
+    task: "Css",
+  },
+  {
+    id: 3,
+    task: "Javascript",
+    courses: [
+      {
+        title: " Learn JavaScript by Building 7 Games - Full Course ",
+        link: "https://www.youtube.com/watch?v=ec8vSKJuZTk&t=990s",
+        type: "project",
+      },
+    ],
+  },
+  {
+    id: 4,
+    task: "TypeScript",
+    docs: [
+      {
+        link: "https://www.typescriptlang.org",
+      },
+    ],
+    courses: [
+      {
+        title: "TypeScript Crash Course 2021",
+        link: "https://www.youtube.com/watch?v=4XLM2ZvtnlA",
+        type: "lesson",
+      },
+    ],
+  },
+  {
+    id: 4,
+    task: "React,Vue, Angular",
+    courses: [
+      {},
+      {
+        title:
+          " Complete Full Functional E-commerce website using React JS | React JS website | #ecommerce #reactjs ",
+        link: "https://www.youtube.com/watch?v=054qYbsxyXw",
+        type: "project",
+      },
+    ],
+  },
+  {
+    id: 5,
+    task: "Next.js / Nuxt.js / Angular Universal:",
+    courses: [
+      {
+        title:
+          "Full-Stack Blog App Tutorial | Next.js MongoDB Blog App Project Full Course",
+        link: "https://www.youtube.com/watch?v=DpYE5zPDRVQ",
+        type: "project",
+      },
+      {
+        title:
+          "Full Stack Next.js, Typescript, Firebase Tutorial – Google Drive Clone",
+        link: "https://www.youtube.com/watch?v=gwOVynGnDZA",
+        type: "project",
+      },
+      {
+        title:
+          "Build a Video Streaming App Using The T3 Stack - Next.js, React, Tailwind, Prisma, TypeScript",
+        link: "https://www.youtube.com/watch?v=RjvgppYXBOI",
+        type: "project",
+      },
+    ],
+  },
+  {
+    id: 6,
+    task: "Redux / Vuex / MobX / Zustand / Valtio / Jotai",
+  },
+  {
+    id: 7,
+    task: "Sass / Less / Stylus",
+  },
+  {
+    id: 8,
+    task: "Bootstrap / Tailwind CSS / Material UI",
+  },
+  {
+    id: 9,
+    task: "styled-components / Emotion",
+  },
+  {
+    id: 10,
+    task: "A11Y",
+  },
+  {
+    id: 11,
+    task: "Responsive Design",
+  },
+  {
+    id: 12,
+    task: "Webpack / Rollup / Parcel / Vite",
+  },
+  {
+    id: 13,
+    task: "Jest / Mocha / Jasmine",
+  },
+  {
+    id: 14,
+    task: "Cypress / Playwright / Puppeteer",
+  },
+  {
+    id: 15,
+    task: "Node js",
+  },
+  {
+    id: 16,
+    task: "NestJs",
+    courses: [
+      {
+        title: "КУРС по NEST.JS - Если писать back-end, тоpug только так!",
+        link: "https://www.youtube.com/watch?v=C5qzO7tsybQ",
+        type: "overview",
+      },
+    ],
+  },
+  {
+    id: 17,
+    task: "Git",
+  },
+  {
+    id: 18,
+    task: "Docker",
+    articles: [
+      {
+        title: "PHP Websites using Docker Containers with PHP Apache and MySQL",
+        link: "https://www.section.io/engineering-education/dockerized-php-apache-and-mysql-container-development-environment/",
+      },
+      {
+        title: 'Laravel Sail под Windows',
+        link: 'https://habr.com/ru/articles/658753/',
+      }
+    ],
+  },
+  {
+    id: 18,
+    task: "Pug",
+    articles: [
+      {
+        title: "A Guide to the Pug HTML Template Preprocessor",
+        link: "https://www.sitepoint.com/a-beginners-guide-to-pug/",
+      },
+    ],
+  },
+  {
+    id: 19,
+    task: "Laravel",
+    courses: [
+      {
+        title: "Полный курс Laravel",
+        link: "https://www.youtube.com/watch?v=aNcbYt8BykY&t=2429s",
+      },
+    ],
+  },
+  {
+    id: 20,
+    task: "php",
+    articles: [
+        {
+            title: '',
+            link: '',
+        }
+    ]
+  }
+];
